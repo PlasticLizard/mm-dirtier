341eb557359b8715171d84aa3e26facf0003c176
PlasticLizard <hereiam@sonic.net>
2010-10-09 21:35:16 -0700

    Removed the monkey patches that were moved into my mm fork

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source 'http://rubygems.org'
 gem 'bson_ext'
 gem 'mongo', '~>1.0.9'
 
-gem 'mongo_mapper', :git=>"http://github.com/jnunemaker/mongomapper.git", :branch=>"rails3"
+gem 'mongo_mapper', :git=>"http://github.com/PlasticLizard/mongomapper.git", :branch=>"rails3"
 
 gemspec
--- lib/mm_dirtier.rb
+++ lib/mm_dirtier.rb
@@ -8,7 +8,6 @@ base_dir = File.dirname(__FILE__)
 ].each {|req| require File.join(base_dir,'mm_dirtier',req)}
 
 require File.join(base_dir,"mongo_mapper","plugins","dirtier")
-require File.join(base_dir,"mongo_mapper","plugins","dirtier_embedded_document")
 
 MongoMapper::Document.append_inclusions(MongoMapper::Plugins::Dirtier)
-MongoMapper::EmbeddedDocument.append_inclusions(MongoMapper::Plugins::DirtierEmbeddedDocument)
+
